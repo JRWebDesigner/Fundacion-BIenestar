9694f2b59e5a05f4cf3ca04fd7e677b3b1ad9944
JRwebdesing <jowebdesigc@gmail.com>
2025-03-24 10:46:54 -0400

    add description SEO

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
-  companyName: 'Fundacion Bienestar',
+  companyName: 'Fundacion Bienestar Bolivia',
   siteUrl: 'https://fundacionbienestarbolivia.com',
   Socials: {
       Email: 'support@titan-core.com',
@@ -10,8 +10,8 @@ export const siteConfig = {
 
 export const SEO = {
   Separator: '|',
-  SiteName: 'Fundacion Bienestar',
-  defaultDescription: 'Fundacion Bienestar',
+  SiteName: 'Fundacion Bienestar Bolivia',
+  defaultDescription: 'Fundacion Bienestar Bolivia con la mision de contribuir de forma integral a mejorar la salud y calidad de vida de las comunidades mediante el desarrollo y ejecucion de diversos proyectos y programas de salud, educacion y deporte',
 };
   
 export const blogSetting = {
