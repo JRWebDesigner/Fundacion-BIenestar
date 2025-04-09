17ca91dd93c74866ad7d858c82a9a65fd03cee64
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-04-09 08:28:48 -0400

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
+import { schema } from "./src/sanity/schemaTypes";
 
 export default defineConfig({
   projectId: "7xk0i7va",
   dataset: "production",
   plugins: [structureTool()],
-  schema: {
-    types: [],
-  },
+  schema,
 });
