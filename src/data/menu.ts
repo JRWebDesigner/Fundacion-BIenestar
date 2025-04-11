64121b1fb71d289f2d218665b11398af0ff5187d
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-04-11 16:21:19 -0400

    Update menu.ts

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -3,13 +3,15 @@
 export const headerMenu = [
     { name: 'Inicio', link: '/'},
     { name: 'Quienes Somos', link: '/Quienessomos' },
-    { name: 'Donde estuvimos', link: '/DondeEstuvimos' }
+    { name: 'Donde estuvimos', link: '/DondeEstuvimos' },
+    {name: "Testimonios", link: '/Testimonios'}
 ];
 
 export const footerMenu = [
     { name: 'Inicio', link: '/'},
     { name: 'Quienes Somos', link: '/Quienessomos' },
-    { name: 'Donde estuvimos', link: '/DondeEstuvimos' }
+    { name: 'Donde estuvimos', link: '/DondeEstuvimos' },
+{name: "Testimonios", link: '/Testimonios'}
 ];
 
 
